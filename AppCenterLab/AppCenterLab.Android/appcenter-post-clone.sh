7eace627d3aed2985b7949bc0331a777c9118cc4
Kazuki Ota <kaota@microsoft.com>
2019-05-14 11:38:48 +0900

    fix path

--- AppCenterLab/AppCenterLab.Android/appcenter-post-clone.sh
+++ AppCenterLab/AppCenterLab.Android/appcenter-post-clone.sh
@@ -3,7 +3,7 @@
 if [ "$APPCENTER_BRANCH" == "master" ];
 then
 	cd 
-	cp $APPCENTER_SOURCE_DIRECTORY/AppCenterConfiguration_sample.txt $APPCENTER_SOURCE_DIRECTORY/AppCenterConfiguration.cs
-	sed -i -e "s/<your android app id>/$ANDROID_ID/g" $APPCENTER_SOURCE_DIRECTORY/AppCenterConfiguration.cs
-	sed -i -e "s/<your ios app id>/$IOS_ID/g" $APPCENTER_SOURCE_DIRECTORY/AppCenterConfiguration.cs
+	cp $APPCENTER_SOURCE_DIRECTORY/AppCenterLab/AppCenterLab.Android/AppCenterConfiguration_sample.txt $APPCENTER_SOURCE_DIRECTORY/AppCenterLab/AppCenterLab.Android/AppCenterConfiguration.cs
+	sed -i -e "s/<your android app id>/$ANDROID_ID/g" $APPCENTER_SOURCE_DIRECTORY/AppCenterLab/AppCenterLab.Android/AppCenterConfiguration.cs
+	sed -i -e "s/<your ios app id>/$IOS_ID/g" $APPCENTER_SOURCE_DIRECTORY/AppCenterLab/AppCenterLab.Android/AppCenterConfiguration.cs
 fi
