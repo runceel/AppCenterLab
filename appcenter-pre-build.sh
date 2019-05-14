c42c15a79fe49c285e4cc5f6563aca1c8b361d24
Kazuki Ota <kaota@microsoft.com>
2019-05-14 11:14:15 +0900

    update prebuild

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,6 +3,6 @@
 if [ "$APPCENTER_BRANCH" == "master" ];
 then
 	cp AppCenterLab/AppCenterConfiguration_sample.txt AppCenterLab/AppCenterConfiguration.cs
-	sed -i -e "s/<your android app id>/$ANDROID_ID/g"
-	sed -i -e "s/<your ios app id>/$IOS_ID/g"
+	sed -i -e "s/<your android app id>/$ANDROID_ID/g" AppCenterLab/AppCenterConfiguration.cs
+	sed -i -e "s/<your ios app id>/$IOS_ID/g" AppCenterLab/AppCenterConfiguration.cs
 fi
